4633fbac18c801096c6318752dcd5717cf649603
Jim Öqvist <44175368+jimoq@users.noreply.github.com>
2024-05-15 22:47:23 +0000

    referd to module

--- 02-tf-import/main.tf
+++ 02-tf-import/main.tf
@@ -5,7 +5,7 @@ data "checkpoint_management_show_objects" "query" {
 
 # terraform plan -generate-config-out=imported-nets.tf
 import {
-    to = checkpoint_management_host.imported
+    to = module.policy.checkpoint_management_host.imported
     id = data.checkpoint_management_show_objects.query.objects[0].uid
 }
 
